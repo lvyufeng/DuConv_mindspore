431519ef6c5ef97c88d09e9fe59af258e2465038
lvyufeng <lvyufeng@cqu.edu.cn>
2021-10-20 07:36:09 +0000

    update scripts

--- README.md
+++ README.md
@@ -8,6 +8,19 @@
 pip install mindspore-gpu==1.5.0
 ```
 
+#### 下载和预处理数据
+
+```bash
+# download dataset
+bash scripts/download_dataset.sh
+# build dataset
+bash scripts/build_dataset.sh [task_type]
+# convert to mindrecord
+bash scripts/convert_dataset.sh [task_type]
+# task type: match, match_kn, match_kn_gene
+# default: match_kn_gene
+```
+
 #### GPU处理器上运行
 
 ```bash
--- scripts/convert_dataset.sh
+++ scripts/convert_dataset.sh
@@ -1,21 +1,41 @@
 #!/bin/bash
-# test dataset
-python src/reader.py --task_name=match_kn_gene \
-                     --max_seq_len=256 \
-                     --vocab_path=data/gene.dict \
-                     --input_file=data/build.train.txt \
-                     --output_file=../../train.mindrecord
-python src/reader.py --task_name=match_kn_gene \
-                     --max_seq_len=256 \
-                     --vocab_path=data/gene.dict \
-                     --input_file=data/build.dev.txt \
-                     --output_file=../../dev.mindrecord
-python src/reader.py --task_name=match_kn_gene \
-                     --max_seq_len=256 \
-                     --vocab_path=data/gene.dict \
-                     --input_file=data/build.test.txt \
-                     --output_file=../../test.mindrecord
+if [ $# -ne 1 ]
+then
+    echo "=============================================================================================================="
+    echo "Please run the script as: "
+    echo "bash convert_dataset.sh [TASK_NAME]"
+    echo "for example: sh scripts/convert_dataset.sh match_kn_gene"
+    echo "TASK_TYPE including [match, match_kn, match_kn_gene]"
+    echo "=============================================================================================================="
+exit 1
+fi
+TASK_NAME=$1
 
-mv ../../train.mindrecord* ./data
-mv ../../dev.mindrecord* ./data
-mv ../../test.mindrecord* ./data
+case $TASK_NAME in
+    "match")
+        DICT_NAME="data/char.dict"
+        ;;
+    "match_kn")
+        DICT_NAME="data/char.dict"
+        ;;
+    "match_kn_gene")
+        DICT_NAME="data/gene.dict"
+        ;;
+    esac
+
+python src/reader.py --task_name=${TASK_NAME} \
+                     --max_seq_len=256 \
+                     --vocab_path=${DICT_NAME} \
+                     --input_file=data/build.train.txt \
+                     --output_file=data/train.mindrecord
+python src/reader.py --task_name=${TASK_NAME} \
+                     --max_seq_len=256 \
+                     --vocab_path=${DICT_NAME} \
+                     --input_file=data/build.dev.txt \
+                     --output_file=data/dev.mindrecord
+python src/reader.py --task_name=${TASK_NAME} \
+                     --max_seq_len=256 \
+                     --vocab_path=${DICT_NAME} \
+                     --input_file=data/build.test.txt \
+                     --output_file=data/test.mindrecord
+
